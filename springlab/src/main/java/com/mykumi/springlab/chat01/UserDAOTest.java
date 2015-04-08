bd16e31ee53e76630205de51bdebe18145a54c80
mykumi <mykumi@gmail.com>
2015-04-08 17:40:45 +0900

    1.8_DI_Using_XML 설정된 XML을 읽어서 ApplicationContext를 생성하도록 테스트코드변경

--- springlab/src/main/java/com/mykumi/springlab/chat01/UserDAOTest.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/UserDAOTest.java
@@ -3,13 +3,14 @@ package com.mykumi.springlab.chat01;
 import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDAOTest {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		//UserDAO userDao = new DaoFactory().userDao();
-		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		ApplicationContext context = new GenericXmlApplicationContext("/com/mykumi/springlab/chat01/applicationContext.xml");
 		UserDAO userDao = context.getBean("userDao", UserDAO.class);
 		
 		User user = new User("mykumi", "UC JUNG");
