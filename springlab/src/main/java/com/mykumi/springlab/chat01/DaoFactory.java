85b4ff5a6cd215bc1fe77aa90a6144f2069b8c6a
mykumi <mykumi@gmail.com>
2015-04-08 18:05:24 +0900

    using DaoFactory

--- springlab/src/main/java/com/mykumi/springlab/chat01/DaoFactory.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/DaoFactory.java
@@ -1,20 +1,27 @@
 package com.mykumi.springlab.chat01;
 
+import javax.sql.DataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
 	@Bean
 	public UserDAO userDao() {
 		UserDAO userDAO = new UserDAO();
-		userDAO.setConnectionMaker(connectionMaker());
+		userDAO.setDataSource(dataSource());
 		return userDAO;
 	}
 
 	@Bean
-	public ConnectionMaker connectionMaker() {
-		ConnectionMaker connectionMaker = new NConnectionMaker();
-		return connectionMaker;
+	public DataSource dataSource() {
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+		dataSource.setUrl("jdbc:mysql://54.64.47.206/mykumidb");
+		dataSource.setUsername("dbuser");
+		dataSource.setPassword("dbuser1*");
+		
+		return dataSource;
 	}
 }
