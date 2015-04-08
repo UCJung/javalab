9240591e48bb2ce5cafd613d9bc1a88321b0b63d
mykumi <mykumi@gmail.com>
2015-04-08 18:19:20 +0900

    Chapter DI Complete - 최종 소스 정리

--- springlab/src/main/java/com/mykumi/springlab/chat01/UserDAOTest.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/UserDAOTest.java
@@ -3,8 +3,6 @@ package com.mykumi.springlab.chat01;
 import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDAOTest {
