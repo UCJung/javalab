68cb83d5e06594bbf12efaecb099c8b2072328ed
mykumi <mykumi@gmail.com>
2015-04-08 13:25:59 +0900

    chat01. using interface : create concreate class

---
+++ springlab/src/main/java/com/mykumi/springlab/chat01/NConnectionMaker.java
@@ -0,0 +1,19 @@
+package com.mykumi.springlab.chat01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class NConnectionMaker implements ConnectionMaker {
+
+	@Override
+	public Connection makeNewConnection() throws ClassNotFoundException,
+			SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection dbConnection = DriverManager.getConnection(
+				"jdbc:mysql://54.64.47.206/mykumidb", 
+				"dbuser", 
+				"dbuser1*");
+		return dbConnection;
+	}
+}
