2eaaa0db3100e8da96667ab24c663e6ff81e9b65
mykumi <mykumi@gmail.com>
2015-04-09 17:50:20 +0900

    2.4.1 테스트를 위한 애플리케이션 컨텍스트관리(3) 테스트 코드에 의한 DI
    특정 테스트에서만 ApplicationContext에서 주입 받은 객체의 상태를 변경하여야
    하는 경우가 발생할 경우 사용할 수 있는 방식
    
    - 테스트를 위하여 별도의 Test 클래스 파일을 생성함.
    - @DirtiesContext Annotation 
    -- 아래의 Class에서는 DI 받은 객체의 상태를 변경함을 알림
    -- 의미 : 기본적으로 ApplicationContext를 사용하지만 해당 클래스만 별도의 Context를 사용함.
    - setUp 에서 주입받은 userDap의 Datasource를 TestDB로 변경한
    SingleConectionDatasource를 생성하여 주입함

---
+++ springlab/src/main/java/com/mykumi/springlab/chat01/UserDAOCodeDITest.java
@@ -0,0 +1,74 @@
+package com.mykumi.springlab.chat01;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.datasource.SingleConnectionDataSource;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations="/com/mykumi/springlab/chat01/applicationContext.xml")
+@DirtiesContext
+public class UserDAOCodeDITest {
+	@Autowired
+	private UserDAO userDao;
+
+	@Before
+	public void setUp() {
+		DataSource dataSource = new SingleConnectionDataSource(
+				"jdbc:mysql://54.64.47.206/mykumitestdb","dbuser", "dbuser1*",true);
+		userDao.setDataSource(dataSource);
+	}
+	
+	@Test
+	public void addAndGet() throws SQLException {
+		System.out.println(this.userDao);
+		userDao.deleteAll();
+		assertThat(userDao.getCount(), is(0));
+		
+		User user = new User("mykumi", "UC JUNG", "111111");
+
+		userDao.add(user);
+		assertThat(userDao.getCount(), is(1));
+		
+		User user2 = userDao.get(user.getId());
+		
+		assertThat(user.getName(), is(user2.getName()));
+		assertThat(user.getPassword(), is(user2.getPassword()));
+	}
+
+	@Test 
+	public void getCount() throws SQLException {
+		userDao.deleteAll();
+		assertThat(userDao.getCount(), is(0));
+		
+		userDao.add(new User("u1", "user01", "111111"));
+		assertThat(userDao.getCount(), is(1));
+		
+		userDao.add(new User("u2", "user02", "222222"));
+		assertThat(userDao.getCount(), is(2));
+		
+		userDao.add(new User("u3", "user03", "333333"));
+		assertThat(userDao.getCount(), is(3));
+	}
+	
+	@Test(expected=EmptyResultDataAccessException.class)
+	public void getFailure() throws SQLException {
+		userDao.deleteAll();
+		assertThat(userDao.getCount(), is(0));
+		
+		userDao.get("unknown_id");
+	}
+}
