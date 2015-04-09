ec616164e24376c20cb27ef68ae879a45968dee1
mykumi <mykumi@gmail.com>
2015-04-09 12:00:57 +0900

    2.2.2. improve test verification (1) - 기존 main method를 Junit Test 형태로 변경 - JUnit 테스트 메소드 생성 주의사항   ' 접근자를 Public으로 설정   ' 반환타입은 void로 설정   ' @Test annotation 부여

--- springlab/src/main/java/com/mykumi/springlab/chat01/UserDAOTest.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/UserDAOTest.java
@@ -1,5 +1,10 @@
 package com.mykumi.springlab.chat01;
 
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
 import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
@@ -7,24 +12,19 @@ import org.springframework.context.support.GenericXmlApplicationContext;
 
 public class UserDAOTest {
 
-	public static void main(String[] args) throws SQLException {
+	@Test
+	public void addAndGet() throws SQLException {
 		ApplicationContext context = new GenericXmlApplicationContext("/com/mykumi/springlab/chat01/applicationContext.xml");
 		UserDAO userDao = context.getBean("userDao", UserDAO.class);
 		
 		User user = new User("mykumi", "UC JUNG");
 		user.setPassword("111111");
+
 		userDao.add(user);
-		System.out.println(user.getId() + " success adding");
 		
 		User user2 = userDao.get(user.getId());
-		if (!user.getName().equals(user2.getName())){
-			System.out.println("Fail Test (name)");
-		} 
-		else if (!user.getPassword().equals(user2.getPassword())) {
-			System.out.println("Fail Test (password)");
-		}
-		else {
-			System.out.println(user2.getId() + " success getting");
-		}
+		
+		assertThat(user.getName(), is(user2.getName()));
+		assertThat(user.getPassword(), is(user2.getPassword()));
 	}
 }
