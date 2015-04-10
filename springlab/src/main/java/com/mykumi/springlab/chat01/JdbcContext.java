55d2e7611dc0de301a20e1e6e01b994ba58ef518
mykumi <mykumi@gmail.com>
2015-04-10 16:23:21 +0900

    [addtional] UserDAO 잔여 메소드 처리 : add(3) - 인자를 String이 아닌 Object로 처리 - Object의 유형에 따라 파라미터를 설정해 주도록 코드 변경
    [TODO] ResultSet 반환값이 있는 나머지 두개 메소드를 처리해 보자.
    - Context가 Method와 약간 다르다.

--- springlab/src/main/java/com/mykumi/springlab/chat01/JdbcContext.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/JdbcContext.java
@@ -6,6 +6,10 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.springframework.jdbc.core.SqlParameterValue;
+import org.springframework.jdbc.core.SqlTypeValue;
+import org.springframework.jdbc.core.StatementCreatorUtils;
+
 public class JdbcContext {
 	private DataSource dataSource;
 	
@@ -49,17 +53,23 @@ public class JdbcContext {
 		});
 	}	
 	
-	public void excuteSql(final String query, final String... params) throws SQLException {
+	public void excuteSql(final String query, final Object... params) throws SQLException {
 		workWithStatementStrategy(new StatementStrategy(){
 			public PreparedStatement makePreparedStatement(Connection dbConnection)
 					throws SQLException {
 				return setParams(dbConnection.prepareStatement(query), params);
 			}
-			private PreparedStatement setParams(PreparedStatement ps, final String[] params)
+			private PreparedStatement setParams(PreparedStatement ps, Object[] params)
 					throws SQLException {
-				int i = 0;
-				for (String param : params) {
-					ps.setString(++i, param);
+				for (int i = 0; i < params.length ; i ++) {
+					Object param = params[i];
+					if (param instanceof SqlParameterValue) {
+						SqlParameterValue paramValue = (SqlParameterValue) param;
+						StatementCreatorUtils.setParameterValue(ps, i + 1, paramValue, paramValue.getValue());
+					}
+					else {
+						StatementCreatorUtils.setParameterValue(ps, i + 1, SqlTypeValue.TYPE_UNKNOWN, param);
+					}					
 				}
 				return ps;
 			}
