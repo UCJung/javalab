7e9c9a35040d71e2e12c3b0c8dd3f9eba038577e
mykumi <mykumi@gmail.com>
2015-04-15 10:21:19 +0900

    3.5.3 템플릿/콜백의 응용 : 테스트와 try/catch/finally (3-1) ● Refactoring (1)   - 해당 메소드는 파일 Read를 위한 처리부와 한줄씩읽어서 더하는 두가지 기능수행   - 파일을 읽는 부분은 공통적으로 사용될 여지가 있기 때문에 별도 템플릿으로 분리   - 루프를 돌며 더하는 로직부분도 별도 메소드로 분리

--- springlab/src/main/java/com/mykumi/springlab/chat01/Calculator.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/Calculator.java
@@ -7,26 +7,34 @@ import java.io.IOException;
 
 public class Calculator {
 	public int sum(String filepath) throws IOException {
+		return fileReadTemplate(filepath);
+	}
+
+	public int fileReadTemplate(String filepath) throws IOException,
+			NumberFormatException, FileNotFoundException {
 		BufferedReader br = null;
-		Integer numberSum = 0;
+		Integer ret = 0;
 		try {
 			br = new BufferedReader(new FileReader(filepath));
-			String line = null;
-			while ((line = br.readLine()) != null) {
-				numberSum += Integer.valueOf(line); 
-			}
-			return numberSum;
+			ret = calcSumWithBufferedRead(br, ret);
+			return ret;
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			throw e;
 		} finally {
 			if (br != null) {
-				try {
-					br.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
+				try { br.close(); } 
+				catch (Exception e) {}
 			}
 		}
+	}
+
+	public Integer calcSumWithBufferedRead(BufferedReader br, Integer ret)
+			throws IOException, NumberFormatException {
+		String line = null;
+		while ((line = br.readLine()) != null) {
+			ret += Integer.valueOf(line); 
+		}
+		return ret;
 	}
 }
