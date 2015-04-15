d6c6f97b07630c288b0a9ba4948eba113b24eabd
mykumi <mykumi@gmail.com>
2015-04-15 10:07:00 +0900

    3.5.3 템플릿/콜백의 응용 : 테스트와 try/catch/finally (2) ● 테스트를 성공하는 로직작성 ● 파라미터 경로의 파일이 없는 경우 테스트 코드 작성

--- springlab/src/main/java/com/mykumi/springlab/chat01/Calculator.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/Calculator.java
@@ -1,7 +1,32 @@
 package com.mykumi.springlab.chat01;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Calculator {
-	public int sum(String filepath) {
-		return 0;
+	public int sum(String filepath) throws IOException {
+		BufferedReader br = null;
+		Integer numberSum = 0;
+		try {
+			br = new BufferedReader(new FileReader(filepath));
+			String line = null;
+			while ((line = br.readLine()) != null) {
+				numberSum += Integer.valueOf(line); 
+			}
+			return numberSum;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			throw e;
+		} finally {
+			if (br != null) {
+				try {
+					br.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
 	}
 }
--- springlab/src/main/java/com/mykumi/springlab/chat01/CalculatorTest.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/CalculatorTest.java
@@ -3,15 +3,23 @@ package com.mykumi.springlab.chat01;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class CalculatorTest {
 
 	@Test
-	public void sumTest() {
+	public void sumTest() throws IOException {
 		Calculator calculator = new Calculator();
 		int result = calculator.sum(getClass().getResource("numbers.txt").getPath());
 		assertThat(result, is(15));
 	}
-
+	
+	@Test(expected=FileNotFoundException.class)
+	public void sumTestWithFileNotExist() throws IOException {
+		Calculator calculator = new Calculator();
+		calculator.sum("c:\\hellow.txt");
+	}
 }
