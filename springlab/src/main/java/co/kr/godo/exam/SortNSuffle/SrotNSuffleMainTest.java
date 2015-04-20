2ce798a7848639d5fb24f52c6ec595f3a581fb91
mykumi <mykumi@gmail.com>
2015-04-20 13:00:31 +0900

    Refactor01. Sort And Shuffle (2) - 최초의 테스트 Case 작성 - 입력결과와 출력결과를 고정하여 테스트 Case 작성

---
+++ springlab/src/main/java/co/kr/godo/exam/SortNSuffle/SrotNSuffleMainTest.java
@@ -0,0 +1,20 @@
+package co.kr.godo.exam.SortNSuffle;
+
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import org.junit.Test;
+
+public class SrotNSuffleMainTest {
+
+	@Test
+	public void testSrotNSuffle() {
+		String input = "4ac13bd2";
+		String result = new String();
+		SortNSuffle sortNSuffle = new srotNSuffle();
+		
+		result = sortNSuffle.doing(input);
+		
+		assertThat(result, is("1a2b3c4d"));
+	}
+}
