7370f3bb6ecbbd156a30585c811ba51a07617ec3
mykumi <mykumi@gmail.com>
2015-04-20 15:34:46 +0900

    Refactor01. Sort And Shuffle (5) Client (Main) 소스정리 - 리펙토링한 클래스를 사용하는 Client인 main 메소드를 정리

--- springlab/src/main/java/co/kr/godo/exam/SortNSuffle/SrotNSuffleMain.java
+++ springlab/src/main/java/co/kr/godo/exam/SortNSuffle/SrotNSuffleMain.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class SrotNSuffleMain {
     public static void main(String[] args) {
-        // input string by system io
-        Scanner scanInputString = new Scanner(System.in);
+        String inputString = getSystemInputString();
+        SortNSuffle sortNSuffle = new SortNSuffle();
+        System.out.println(sortNSuffle.doing(inputString, new NumberCharacterShuffleStrategy()));
+    }
+
+	@SuppressWarnings("resource")
+	public static String getSystemInputString() {
+		Scanner scanInputString = new Scanner(System.in);
         System.out.println("String을 입력하세요 : ");
         String inputString = scanInputString.nextLine();
-        SortNSuffle sortNSuffle = new SortNSuffle();
-        
-        String result = sortNSuffle.doing(inputString, new NumberCharacterShuffleStrategy());
-        System.out.println(result);
-    }
+		return inputString;
+	}
 }
