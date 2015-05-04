87449f04f7a8081478ac51d033f5bd826d2e3c93
mykumi <mykumi@gmail.com>
2015-05-04 16:07:13 +0900

    [디자인패턴] 데코레이터 패턴 구현 (4) 1. ApplicationContext를 통하여 Client에서 DI 받도록 구현    - ApplicationContext 파일 생성      ' iComponent bean 설정 2. Client Main 로직 변경    - ApplicationContext에 설정된 Bead에 의하여 구체클래스를 DI 받도록 처리
    [변경점]
    - Application Context에 의하여 어떤 구체클래스를 사용할지 결정됨
    - Client의 중복이 사라짐.    
    
    [다음할일]
    - WrapperClass가 하나가더 늘었다고 가정해 보자.

--- springlab/src/main/java/co/kr/godo/decorator/Client.java
+++ springlab/src/main/java/co/kr/godo/decorator/Client.java
@@ -1,8 +1,17 @@
 package co.kr.godo.decorator;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 public class Client {
+	private static IComponent iComponent;
+	
 	public static void main(String[] args) {
-		IComponent iComponent = new OrignalClass();
+		@SuppressWarnings("resource")
+		ApplicationContext context = new GenericXmlApplicationContext(
+				"/co/kr/godo/decorator/applicationContext.xml");
+		iComponent = context.getBean("iComponent", IComponent.class);
+		
 		iComponent.setFirstName("Jung");
 		iComponent.setLastName("Woochul");
 		System.out.println(iComponent.getWelcomeMessage());
