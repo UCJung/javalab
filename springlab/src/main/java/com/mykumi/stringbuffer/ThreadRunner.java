e076ce954998cad31499a882bb4feb26c7a0e9cd
mykumi <mykumi@gmail.com>
2015-05-20 11:06:49 +0900

    [Tip] String Builder와 String Buffer 용법에 대한 예제 - Multi Thread를 통하여 String Bulider와 String Buffer의 차이를 확인한다.
    [학습내용]
    - StringBuilder와 StringBuffer의 차이점
    - Multi Thread에서 동시성 제어를 위한 synchronized의 이해

---
+++ springlab/src/main/java/com/mykumi/stringbuffer/ThreadRunner.java
@@ -0,0 +1,48 @@
+package com.mykumi.stringbuffer;
+
+public class ThreadRunner implements Runnable {
+	private Thread t;
+	private String threadName; 
+	private StringBuilder sb;
+	private StringBuffer sbf;
+	
+	
+	public ThreadRunner(String threadName, StringBuilder sb, StringBuffer sbf) {
+		this.threadName = threadName;
+		this.sb = sb;
+		this.sbf = sbf;
+		System.out.println("Create : " + this.threadName);
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Running : " + threadName);
+		for (int i = 4; i > 0 ; i--) {
+			for (int j = 0 ; j < 10000 ; j ++ ) {
+				sb.append("a");
+				sbf.append("a");
+			}
+			try {
+				Thread.sleep(0);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+
+		System.out.println("Exited " +  threadName);
+	}
+	
+	public void start() {
+		System.out.println("Strat " + threadName);
+		if (t == null) {
+			t = new Thread(this, threadName);
+			t.start();
+		}
+	}
+	
+	public boolean isThreadAlive() {
+		return this.t.isAlive();
+	}
+}
---
+++ springlab/src/main/java/com/mykumi/stringbuffer/ThreadRunnerTest.java
@@ -0,0 +1,70 @@
+package com.mykumi.stringbuffer;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class ThreadRunnerTest {
+
+	private static ArrayList<ThreadRunner> threadRunners;
+	private static StringBuilder sb;
+	private static StringBuffer sbf;	
+	
+	@Test
+	public final void testStringBuilder() throws InterruptedException {
+		sb = new StringBuilder();
+		sbf = new StringBuffer();	
+		
+		createRunners();
+		activatedRunners();
+		isAliveRunners();
+
+		assertEquals(160000, sb.toString().length());
+	}
+	
+	@Test
+	public final void testStringBuffer() throws InterruptedException {
+		sb = new StringBuilder();
+		sbf = new StringBuffer();	
+		
+		createRunners();
+		activatedRunners();
+		isAliveRunners();
+
+		assertEquals(160000, sbf.toString().length());
+	}	
+	
+	private void activatedRunners() {
+		for (ThreadRunner tr : threadRunners) {
+			tr.start();
+		}
+	}	
+	
+	private void createRunners() {
+		threadRunners = new ArrayList<ThreadRunner>();
+		
+		threadRunners.add(new ThreadRunner("Thread-1", sb, sbf));
+		threadRunners.add(new ThreadRunner("Thread-2", sb, sbf));
+		threadRunners.add(new ThreadRunner("Thread-3", sb, sbf));
+		threadRunners.add(new ThreadRunner("Thread-4", sb, sbf));
+	}	
+	
+	private void isAliveRunners() throws InterruptedException {
+		while(true) {
+			boolean isAnyOneAlive = false;
+			for (ThreadRunner tr : threadRunners) {
+				if (tr.isThreadAlive() == true) {
+					isAnyOneAlive = true;
+					break;
+				}
+			}
+			if (isAnyOneAlive == false) break;
+			Thread.sleep(1);
+		}
+		System.out.println(" String Builder Length " +  sb.toString().length());
+		System.out.println(" String Buffer Length " +  sbf.toString().length());		
+		System.out.println("All Runner Exited");
+	}
+}
