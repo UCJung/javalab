a2848dc3b999281c0353e509abd2be7051008178
mykumi <mykumi@gmail.com>
2015-06-22 11:24:46 +0900

    5.1.3 UserService.upgradeLevels (2) 전체 사용자의 정보로 일괄적으로 레벨을 조정하는 upgradeLevels 메소드 추가
    - UserService Class : upgradeLevels Method 추가
    - UserServiceTest Class : upgradeLevels Text Method 추가

--- springlab/src/main/java/com/mykumi/springlab/chat01/user/service/UserService.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/user/service/UserService.java
@@ -1,5 +1,9 @@
 package com.mykumi.springlab.chat01.user.service;
 
+import java.util.List;
+
+import com.mykumi.springlab.chat01.Level;
+import com.mykumi.springlab.chat01.User;
 import com.mykumi.springlab.chat01.UserDao;
 
 public class UserService {
@@ -7,5 +11,31 @@ public class UserService {
 	
 	public void setUserDao(UserDao userDao) {
 		this.userDao = userDao;
+	}
+	
+	public void upgradeLevels() {
+		List<User> users = userDao.getAll();
+		
+		for (User user : users) {
+			Boolean changed = null;	// 레벨의 변화가 있는지 확인하는 플래그
+			if (user.getLevel() == Level.BASIC && user.getLogin() >= 50) {
+				user.setLevel(Level.SILVER);
+				changed = true;
+			}
+			else if (user.getLevel() == Level.SILVER && user.getRecommend() >= 30) {
+				user.setLevel(Level.GOLD);
+				changed = true;
+			}
+			else if (user.getLevel() == Level.GOLD) {
+				changed = false;
+			}
+			else {
+				changed = false;
+			}
+			
+			if (changed) {
+				userDao.update(user);
+			}
+		}
 	}
 }
--- springlab/src/main/java/com/mykumi/springlab/chat01/user/service/UserServiceTest.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/user/service/UserServiceTest.java
@@ -3,11 +3,21 @@ package com.mykumi.springlab.chat01.user.service;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.mykumi.springlab.chat01.Level;
+import com.mykumi.springlab.chat01.User;
+import com.mykumi.springlab.chat01.UserBuilder;
+import com.mykumi.springlab.chat01.UserBuilderTypeA;
+import com.mykumi.springlab.chat01.UserDao;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations="/com/mykumi/springlab/chat01/user/service/applicationContext.xml")
@@ -15,8 +25,47 @@ public class UserServiceTest {
 	@Autowired
 	UserService userService;
 	
+	@Autowired
+	UserDao userDao;
+	
+	List<User> users;
+	
+	@Before
+	public void setUp() {
+		UserBuilder userBuilder = new UserBuilderTypeA();
+		users = Arrays.asList(
+				userBuilder.id("user1").name("name1").password("u1").level(Level.BASIC).login(49).recommend(0).build(),
+				userBuilder.id("user2").name("name2").password("u2").level(Level.BASIC).login(50).recommend(0).build(),
+				userBuilder.id("user3").name("name3").password("u3").level(Level.SILVER).login(60).recommend(29).build(),
+				userBuilder.id("user4").name("name4").password("u4").level(Level.SILVER).login(60).recommend(30).build(),
+				userBuilder.id("user5").name("name5").password("u5").level(Level.GOLD).login(100).recommend(100).build()
+				);
+	}
+	
 	@Test
 	public void bean() {
 		assertThat(this.userService, is(notNullValue()));
+	}
+	
+	@Test
+	public void ugradeLevels() {
+		userDao.deleteAll();
+		
+		for (User user : users) {
+			userDao.add(user);
+		}
+		
+		userService.upgradeLevels();
+		
+		checkLevel(users.get(0), Level.BASIC);
+		checkLevel(users.get(1), Level.SILVER);
+		checkLevel(users.get(2), Level.SILVER);
+		checkLevel(users.get(3), Level.GOLD);
+		checkLevel(users.get(4), Level.GOLD);
+	}
+	
+	private void checkLevel(User user, Level expectedLevel) {
+		User userUpdate = userDao.get(user.getId());
+		assertThat(userUpdate.getLevel(), is(expectedLevel));
 	}
 }
