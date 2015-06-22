d52ad1c133f210a7ff093c7e0b46a9116450dc98
mykumi <mykumi@gmail.com>
2015-06-22 16:40:59 +0900

    5.4.1 JavMail을 이용한 메일 발송 기능 Level이 업그레이든 된 사용자에게 메일 발송 기능 추가 - pom.xml : JavaMail 의존성 추가 (V1.4) - UserSerive에 안내메일 발송메소드 추가 - upgradeLevel에서 메일 발송 메소드 호출
    [테스트오류]
    upgradeLevel이 호출될 경우 메일 발송 오류가 발생함

--- springlab/src/main/java/com/mykumi/springlab/chat01/user/service/UserService.java
+++ springlab/src/main/java/com/mykumi/springlab/chat01/user/service/UserService.java
@@ -1,6 +1,16 @@
 package com.mykumi.springlab.chat01.user.service;
 
+import java.io.UnsupportedEncodingException;
 import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.TransactionStatus;
@@ -9,6 +19,7 @@ import org.springframework.transaction.support.DefaultTransactionDefinition;
 import com.mykumi.springlab.chat01.Level;
 import com.mykumi.springlab.chat01.User;
 import com.mykumi.springlab.chat01.UserDao;
+import com.mysql.jdbc.interceptors.SessionAssociationInterceptor;
 
 public class UserService {
 	public static final int MIN_RECOMMEND_FOR_GOLD = 30;
@@ -63,6 +74,27 @@ public class UserService {
 	protected void upgradeLevel(User user) {
 		user.upgradeLevel();
 		userDao.update(user);
+		sendUpgradeEMail(user);
+	}
+
+	private void sendUpgradeEMail(User user) {
+		Properties props = new Properties();
+		props.put("mail.smtp.host", "mail.ksug.org");
+		Session s = Session.getInstance(props, null);
+		
+		MimeMessage message = new MimeMessage(s);
+		try {
+			message.setFrom(new InternetAddress("useradmin@ksug.org"));
+			message.addRecipient(Message.RecipientType.TO, new InternetAddress(user.getEmail()));
+			message.setSubject("Upgrade 안내");
+			message.setText("사용자님의 등급이 " + user.getLevel().name() +"로 업그레이드 되었습니다.");
+			
+			Transport.send(message);
+		} catch (AddressException e) {
+			throw new RuntimeException(e);
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		} 
 	}
 
 	// 레벨이 설정되어 있지 않은 경우 BASIC 레벨로 설정한다.
