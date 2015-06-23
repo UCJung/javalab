3cfb7bbf36f8bcb29066562f1756e088bab0013c
mykumi <mykumi@gmail.com>
2015-06-23 10:23:53 +0900

    6.3.2 다이내믹 프록시 - 다이내믹 프록시 확장 - Target Object의 Return Type이나 수행되어야할 Method에 선별적 기능적용

--- springlab/src/main/java/com/mykumi/springlab/exam/dynamicproxy/UppercaseHandler.java
+++ springlab/src/main/java/com/mykumi/springlab/exam/dynamicproxy/UppercaseHandler.java
@@ -4,17 +4,21 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 
 public class UppercaseHandler implements InvocationHandler {
-	Hello target;
+	Object target;
 	
-	public UppercaseHandler(Hello target) {
+	public UppercaseHandler(Object target) {
 		this.target = target;
 	}
 	
 	@Override
 	public Object invoke(Object proxy, Method method, Object[] args)
 			throws Throwable {
-		String ret = (String)method.invoke(target, args);
-		return ret.toUpperCase();
+		Object ret = method.invoke(target, args);
+		if (ret instanceof String && method.getName().startsWith("say")) {
+			return ((String)ret).toUpperCase();
+		} else {
+			return ret;
+		}
 	}
 
 }
