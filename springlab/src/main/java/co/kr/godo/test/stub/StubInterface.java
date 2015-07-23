88d6c2ac8e5fa9a8d33b138ba1602e2650d76337
mykumi <mykumi@gmail.com>
2015-07-23 12:19:46 +0900

    소스정리

--- springlab/src/main/java/co/kr/godo/test/stub/StubInterface.java
+++
@@ -1,7 +0,0 @@
-package co.kr.godo.test.stub;
-
-public interface StubInterface {
-	public Integer plus(int[] integers);
-	public Integer minus(int[] integers);
-	public Integer multifly(int[] integers);
-}
