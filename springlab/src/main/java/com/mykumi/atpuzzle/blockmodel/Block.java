840a28ad4d045c0995480e9daa2125a532fb56e7
mykumi <mykumi@gmail.com>
2015-07-24 13:03:52 +0900

    [puzzle] add Position Property and method

--- springlab/src/main/java/com/mykumi/atpuzzle/blockmodel/Block.java
+++ springlab/src/main/java/com/mykumi/atpuzzle/blockmodel/Block.java
@@ -2,10 +2,23 @@ package com.mykumi.atpuzzle.blockmodel;
 
 public abstract class Block {
 	protected BlockType blockType; 
+	protected Position position;
 	
 	public abstract void display();
 
 	public BlockType getBlockType() {
 		return blockType;
+	}
+	
+	public void setPosition(int x, int y) {
+		if (position == null) {
+			this.position = new Position();
+		}
+		this.position.setX(x);
+		this.position.setY(y);
+	}
+	
+	public Position getPosition() {
+		return this.position;
 	}
 }
