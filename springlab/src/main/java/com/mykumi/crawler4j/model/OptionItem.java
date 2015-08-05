02dd58e5424d83c4897a0536e3b9b7ce8fbe8e55
mykumi <mykumi@gmail.com>
2015-08-05 09:01:02 +0900

    crawler - add Option item Model

---
+++ springlab/src/main/java/com/mykumi/crawler4j/model/OptionItem.java
@@ -0,0 +1,26 @@
+package com.mykumi.crawler4j.model;
+
+public class OptionItem {
+	private String optionValue;
+	private String optionText;
+	private float optionPrice;
+	
+	public String getOptionValue() {
+		return optionValue;
+	}
+	public void setOptionValue(String optionValue) {
+		this.optionValue = optionValue;
+	}
+	public String getOptionText() {
+		return optionText;
+	}
+	public void setOptionText(String optionText) {
+		this.optionText = optionText;
+	}
+	public float getOptionPrice() {
+		return optionPrice;
+	}
+	public void setOptionPrice(float optionPrice) {
+		this.optionPrice = optionPrice;
+	}
+}
